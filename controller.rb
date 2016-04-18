d74c89ec12b6d91d24b5542e6cedca63ca3ab33d
Yohan Martin <yohanmartin3@gmail.com>
2016-04-18 11:41:26 +0530

    removed commented lines

--- controller.rb
+++ controller.rb
@@ -9,69 +9,67 @@ def initialize
   @db_conn = DB.new
 end
 
-# sinatra_thread = Thread.new do
-  get '/' do
-    [404, 'oops']
-  end
+get '/' do
+  [404, 'oops']
+end
 
-  get '/generate-keys' do
-    api = Api.new @db_conn
-    number_of_keys_to_generate = params['number_of_keys']
-    begin
-      if number_of_keys_to_generate
-        number_of_keys_generated = api.generate_keys(number_of_keys_to_generate.to_i)
-      else
-        number_of_keys_generated = api.generate_keys
-      end
-      [200, "#{number_of_keys_generated} keys generated"]    
-    rescue StandardError => e
-      [422, "#{e}"]
-    end
-  end
-
-  get '/get-key' do
-    api = Api.new @db_conn
-    key = api.allocate_key
-    if key
-      [200, key]
+get '/generate-keys' do
+  api = Api.new @db_conn
+  number_of_keys_to_generate = params['number_of_keys']
+  begin
+    if number_of_keys_to_generate
+      number_of_keys_generated = api.generate_keys(number_of_keys_to_generate.to_i)
     else
-      [404, 'No key available']
+      number_of_keys_generated = api.generate_keys
     end
+    [200, "#{number_of_keys_generated} keys generated"]    
+  rescue StandardError => e
+    [422, "#{e}"]
   end
+end
 
-  get '/unblock' do
-    key = params['key']
-    api = Api.new @db_conn
-    begin
-      api.unblock_key(key)
-      [200, 'Unblocked']
-    rescue StandardError => e
-      [404, "#{e}"]
-    end
+get '/get-key' do
+  api = Api.new @db_conn
+  key = api.allocate_key
+  if key
+    [200, key]
+  else
+    [404, 'No key available']
   end
+end
 
-  get '/delete' do
-    key = params['key']
-    api = Api.new @db_conn
-    begin
-      api.delete_key(key)
-      [200, 'deleted']
-    rescue StandardError => e
-      [404, "#{e}"]
-    end
+get '/unblock' do
+  key = params['key']
+  api = Api.new @db_conn
+  begin
+    api.unblock_key(key)
+    [200, 'Unblocked']
+  rescue StandardError => e
+    [404, "#{e}"]
   end
+end
 
-  get '/keep-alive' do
-    key = params['key']
-    api = Api.new @db_conn
-    begin
-      api.keep_alive(key)
-      [200, 'Updated Keep Alive time']
-    rescue StandardError => e
-      [404, "#{e}"]
-    end
+get '/delete' do
+  key = params['key']
+  api = Api.new @db_conn
+  begin
+    api.delete_key(key)
+    [200, 'deleted']
+  rescue StandardError => e
+    [404, "#{e}"]
   end
-# end
+end
+
+get '/keep-alive' do
+  key = params['key']
+  api = Api.new @db_conn
+  begin
+    api.keep_alive(key)
+    [200, 'Updated Keep Alive time']
+  rescue StandardError => e
+    [404, "#{e}"]
+  end
+end
 
 
 keep_alive_thread = Thread.new do
